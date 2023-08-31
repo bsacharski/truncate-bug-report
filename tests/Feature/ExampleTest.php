4008a7a4a0674aee792580ff2f9d38e46407ab0d
Bartlomiej Sacharski <bartlomiej.sacharski@profitroom.com>
2023-08-31 15:16:57 +0200

    Configure project to trigger bug in DatabaseTruncation

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,15 +2,27 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTruncation;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Let's have 2nd test for truncate to actually start doing something
+     */
+    public function test_the_application_returns_a_successful_response2(): void
     {
         $response = $this->get('/');
 
